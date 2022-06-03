34cffd90dfb8a4f3cec8ea8fae7b6209bdad9abd
Drew Payton <drewpayton18@gmail.com>
2022-06-03 15:17:58 -0500

    adding the code

---
+++ Paddle.js
@@ -0,0 +1,34 @@
+const SPEED = .02
+
+
+export default class Paddle {
+    constructor(paddleEl) {
+        this.paddleEl = paddleEl
+        this.reset()
+    }
+
+
+
+get position() {
+    return parseFloat(getComputedStyle(this.paddleEl).getPropertyValue('--position'))
+}
+
+set position(value) {
+    this.paddleEl.style.setProperty('--position', value)
+}
+
+rect() {
+    return this.paddleEl.getBoundingClientRect()
+}
+
+reset() {
+    this.position = 50
+}
+
+update(delta, ballHeight) {
+this.position += SPEED * delta * (ballHeight - this.position)
+}
+
+
+
+}
---
+++ ball.js
@@ -0,0 +1,67 @@
+const INITAL_VELOCITY = .025
+const VELOCITY_INCREASE = 0.00001
+
+
+export default class Ball {
+    constructor(ballEl) {
+        this.ballEl = ballEl
+        this.reset()
+    }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballEl).getPropertyValue('--x'))
+    }
+
+    set x(value) {
+        this.ballEl.style.setProperty('--x', value)
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballEl).getPropertyValue('--y'))
+    }
+
+    set y(value) {
+        this.ballEl.style.setProperty('--y', value)
+    }
+    
+    rect() {
+        return this.ballEl.getBoundingClientRect()
+    }
+
+    reset() {
+        this.x = 50
+        this.y = 50
+        this.direction = { x: 0 }
+        while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9) {
+            const head = randomNumberBetween(0, 2 * Math.PI)
+            this.direction = { x: Math.cos(head), y: Math.sin(head) }
+        }
+        this.velocity = INITAL_VELOCITY
+    }
+
+
+update(delta, paddleRects) {
+this.x += this.direction.x * this.velocity * delta;
+this.y += this.direction.y * this.velocity * delta;
+this.velocity += VELOCITY_INCREASE * delta
+const rect = this.rect()
+
+
+if (rect.bottom >= window.innerHeight || rect.top <=0) {
+    this.direction.y *= -1
+}
+
+if (paddleRects.some(r => isCollision(r, rect))) {
+    this.direction.x *= -1
+}
+
+}
+}
+
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min
+}
+
+function isCollision(rect1, rect2) {
+    return rect1.left <= rect2.right && rect1.right >= rect2.left && rect1.top <= rect2.bottom && rect1.bottom >= rect2.top
+}
